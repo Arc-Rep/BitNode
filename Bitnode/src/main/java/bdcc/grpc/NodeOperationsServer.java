9cc3c2042d70eea21b16fadd09b5b2349bfef542
xazo8 <andreguil.ruano@gmail.com>
2020-05-01 16:28:54 +0100

    a

--- Bitnode/src/main/java/bdcc/grpc/NodeOperationsServer.java
+++ Bitnode/src/main/java/bdcc/grpc/NodeOperationsServer.java
@@ -120,6 +120,7 @@ public class NodeOperationsServer {
             .setUserAddress(server_address)
             .build();
         responseObserver.onNext(reply);
+        //System.out.println("User " + Crypto.toHex(node_id.getUserId()) + " from " + node_id.getUserAddress() + " connected");
         if(node_list != null)           // if there are no other nodes
         {
           for(KeyNode node: node_list)
