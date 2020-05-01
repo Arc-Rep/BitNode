38ebc8821814e7c0e016e1267d934d27245a1259
dorpex23 <rezzik.geikaal@gmail.com>
2020-05-01 15:12:17 +0100

    h

--- Bitnode/src/main/java/bdcc/main/App.java
+++ Bitnode/src/main/java/bdcc/main/App.java
@@ -246,7 +246,7 @@ public class App {
         System.out.println("5 => View my auction status");
         System.out.println("6 => Conclude my auction");
         System.out.println("0 => Exit");
-        System.out.print("Option: ");
+        System.out.println("Option: ");
     }
 
     private static void menusCLI(String address) throws UnknownHostException {
