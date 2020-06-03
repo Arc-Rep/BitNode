384d13baa8a072499f0fd2feac564d59b65623fe
xazo8 <andreguil.ruano@gmail.com>
2020-06-03 15:32:12 +0100

    bugfix

--- Bitnode/src/main/java/bdcc/kademlia/NodeActions.java
+++ Bitnode/src/main/java/bdcc/kademlia/NodeActions.java
@@ -21,6 +21,8 @@ public class NodeActions {
         Auction random_auction = auctions.getRandomAuction(), user_auction = current_user.getUserAuction();
         NodeNotification response = null;
 
+        if(random_auction.getSeller().equals(node.getKey())) random_auction = null;
+
         try
         {   //cipher with node public key
             response = initial_requester.notifyNode(
